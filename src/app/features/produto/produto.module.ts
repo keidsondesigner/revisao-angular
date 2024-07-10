d7fb5c9e09cc907918df982ddbcdc21eeb80d2c6
Keidson Roby <keidsondesigner@gmail.com>
2024-07-10 00:33:05 -0400

    secao 2: Criado Módulo AppMeterial organizar imports  components Angular Material

--- src/app/features/produto/produto.module.ts
+++ src/app/features/produto/produto.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProdutoRoutingModule } from './produto-routing.module';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { ListagemComponent } from './listagem/listagem.component';
+import { AppMaterialModule } from 'src/app/shared/app-material/app-material.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ListagemComponent } from './listagem/listagem.component';
   ],
   imports: [
     CommonModule,
-    ProdutoRoutingModule
+    ProdutoRoutingModule,
+    AppMaterialModule
   ]
 })
 export class ProdutoModule { }
