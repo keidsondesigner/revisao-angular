fe928d4978ea4819b614a04dd69db14acb252026
Keidson Roby <keidsondesigner@gmail.com>
2024-07-09 23:52:09 -0400

    secao 2: Criado Rotas Internas do Módulo de Produto

--- src/app/features/produto/produto-routing.module.ts
+++ src/app/features/produto/produto-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'listagem', pathMatch: 'full'},
+  {path: 'listagem', component: ListagemComponent},
+  {path: 'novo-produto', component: CadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
