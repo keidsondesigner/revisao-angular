118dbfc491c93bbde062b18826664cf436bd103c
Keidson Roby <keidsondesigner@gmail.com>
2024-07-10 12:30:05 -0400

    secao 2: adicionando MatTable

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
   exports: [
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule
   ],
 })
 export class AppMaterialModule { }
