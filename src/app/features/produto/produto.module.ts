929db612c337912c3e3056c921aae7b1f2218847
Keidson Roby <keidsondesigner@gmail.com>
2024-07-09 23:41:18 -0400

    secao 2: Criado Component produto/listagem

--- src/app/features/produto/produto.module.ts
+++ src/app/features/produto/produto.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProdutoRoutingModule } from './produto-routing.module';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CadastroComponent,
+    ListagemComponent
+  ],
   imports: [
     CommonModule,
     ProdutoRoutingModule
